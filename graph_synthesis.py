659a03934b8bf7409254e13a24b77ad0f47856a9
huypham9966 <36774840+huypham9966@users.noreply.github.com>
2018-03-22 21:23:42 -0700

    Add files via upload

--- graph_synthesis.py
+++ graph_synthesis.py
@@ -1,6 +1,8 @@
 import numpy as np 
+import networkx as nx 
+import node2vec as n2v
 
-np.random.seed(5)
+#np.random.seed(5)
 def generate_graphs(num_eg, vertex, edge_dens, label):
     data = {}
     G = np.zeros((num_eg, vertex, vertex))
@@ -20,6 +22,52 @@ def generate_graphs(num_eg, vertex, edge_dens, label):
     data["graphs"] = G
     data["labels"] = np.ones((num_eg,1)) * label 
     return data
+
+def generate_nx_graphs(num_eg, vertex, edge_dens, label):
+    data = {}
+    G = np.zeros((num_eg, vertex, vertex))
+    """for i in range(vertex):
+        for j in range(i,vertex):
+            G[i,j,:] = np.random.randint(2) * np.ones((1,1,num_eg))
+        for j in range(i):
+            G[i,j,:] = G[j,i,:]"""
+    #for i in range(num_eg):
+    #    G[:,:,i] = np.random.randint(2) * np.ones((vertex,vertex,1))
+    for i in range(vertex):
+        for j in range(i,vertex):
+            X = np.random.rand(num_eg) < edge_dens
+            G[:,i,j] = X.astype(float)
+        for j in range(i):
+            G[:,i,j] = G[:,j,i]
+    data["graphs"] = nx.from_numpy_matrix(G)
+    data["labels"] = np.ones((num_eg,1)) * label 
+    return data
+
+def node2vec_graphs(num_eg, vertex, edge_dens, label):
+    data = {}
+    G = np.zeros((num_eg, vertex, vertex))
+    """for i in range(vertex):
+        for j in range(i,vertex):
+            G[i,j,:] = np.random.randint(2) * np.ones((1,1,num_eg))
+        for j in range(i):
+            G[i,j,:] = G[j,i,:]"""
+    #for i in range(num_eg):
+    #    G[:,:,i] = np.random.randint(2) * np.ones((vertex,vertex,1))
+    for i in range(vertex):
+        for j in range(i,vertex):
+            X = np.random.rand(num_eg) < edge_dens
+            G[:,i,j] = X.astype(float)
+        for j in range(i):
+            G[:,i,j] = G[:,j,i]
+    graph = nx.from_numpy_matrix(G)
+    data["labels"] = np.ones((num_eg,1)) * label 
+    node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4)
+    model = node2vec.fit(window=10, min_count=1, batch_words=4)
+    graphdat = np.zeros((64, vertex))
+    for i in range(vertex):
+        graphdat[:,i] = model[i]
+    data["graphs"] = graphdat 
+    return data 
 
 def one_random_graph(vertex, dens1, dens2):
     permutation = list(np.random.permutation(vertex))
@@ -44,6 +92,72 @@ def one_random_graph(vertex, dens1, dens2):
             G[i,j] = np.random.rand() < 1/2
             G[j,i] = G[i,j]
     
+    G = G[:, permutation]
+    G = G[permutation, :] 
+    return G 
+
+def one_random_graph_label(vertex, dens1, dens2):
+    permutation = list(np.random.permutation(vertex))
+    
+    num1 = int(vertex/2)
+    num2 = vertex - num1
+    
+    G = np.zeros((vertex,vertex))
+    label = np.zeros((vertex,1))
+    label[0:num1,:] = np.ones((num1,1))
+    
+    for i in range(num1):
+        for j in range(i):
+            G[i,j] = np.random.rand() < dens1 
+            G[j,i] = G[i,j] 
+    
+    for i in range(num1,vertex):
+        for j in range(i,vertex):
+            G[i,j] = np.random.rand() < dens2 
+            G[j,i] = G[i,j] 
+    
+    for i in range(num1):
+        for j in range(num1,vertex):
+            G[i,j] = np.random.rand() < 1/2
+            G[j,i] = G[i,j]
+    
+    G = G[:, permutation]
+    G = G[permutation, :] 
+    label = label[permutation,:] 
+    return G, label 
+
+def one_random_graph_hard(vertex):
+    permutation = list(np.random.permutation(vertex))
+    num1 = int(vertex/6)
+    num2 = vertex - 5 * num1 
+    G = np.zeros((vertex,vertex))
+    for i in range(vertex):
+        for j in range(i,vertex):
+            G[i,j] = np.random.rand() < 1/3 
+            G[j,i] = G[i,j] 
+    for i in range(num1):
+        for j in range(num1,(num1*2)):
+            G[i,j] = np.random.rand() < 2/3 
+            G[j,i] = G[i,j] 
+        for j in range(num2,vertex):
+            G[i,j] = np.random.rand() < 2/3 
+            G[j,i] = G[i,j] 
+    for i in range(num1, 2*num1):
+        for j in range((2*num1),(num1*3)):
+            G[i,j] = np.random.rand() < 2/3 
+            G[j,i] = G[i,j] 
+    for i in range(2*num1, 3*num1):
+        for j in range((3*num1),(num1*4)):
+            G[i,j] = np.random.rand() < 2/3 
+            G[j,i] = G[i,j]    
+    for i in range(3*num1, 4*num1):
+        for j in range((4*num1),num2):
+            G[i,j] = np.random.rand() < 2/3 
+            G[j,i] = G[i,j]
+    for i in range(4*num1, num2):
+        for j in range((num2),(vertex)):
+            G[i,j] = np.random.rand() < 2/3 
+            G[j,i] = G[i,j]        
     G = G[:, permutation]
     G = G[permutation, :] 
     return G 
@@ -75,3 +189,111 @@ def generate_ensemble_v2(num_eg, vertex):
     data["graphs"][num:num_eg, :, :] = ensemble["graphs"]
     data["labels"][num:num_eg, :] = ensemble["labels"]
     return data 
+
+def generate_ensemble_v3(num_eg, vertex):
+    data = {}
+    data["graphs"] = np.zeros((num_eg, vertex, vertex))
+    data["labels"] = np.zeros((num_eg, 1))
+    
+    num = int(num_eg/2) 
+    
+    for i in range(num):
+        data["graphs"][i, :, :] = one_random_graph_hard(vertex)
+        data["labels"][i, :] = 1
+        
+    ensemble = generate_graphs(num, vertex, 4/9, 0)
+    data["graphs"][num:num_eg, :, :] = ensemble["graphs"]
+    data["labels"][num:num_eg, :] = ensemble["labels"]
+    return data 
+
+def generate_emsemble_n2v(num_eg, vertex):
+    data = {}
+    data["graphs"] = np.zeros((num_eg, 64, vertex))
+    data["labels"] = np.zeros((num_eg, 1))
+    
+    num = int(num_eg/2) 
+    
+    for i in range(num):
+        data["graphs"][i, :, :] = node2vec_graphs(num_eg, vertex, 1/2, label)["graphs"]
+        data["labels"][i, :] = node2vec_graphs(num_eg, vertex, 1/2, label)["labels"]
+        
+    return data 
+
+def augment(data):
+    num_eg = data["graphs"].shape[0]
+    vertex = data["graphs"].shape[1]
+    newdata = np.zeros((num_eg, vertex + 1, vertex + 1))
+    newdata[:,0:vertex,0:vertex] = data["graphs"]
+    newdata[:,vertex,:] = np.ones((num_eg,vertex+1))
+    newdata[:,:,vertex] = np.ones((num_eg,vertex+1))
+    data["graphs"] = newdata
+    return data 
+
+def generate_ensemble_v2_label(num_eg, vertex):
+    data = {}
+    data["graphs"] = np.zeros((num_eg, vertex, vertex))
+    data["labels"] = np.zeros((num_eg, 1))
+    data["vertex"] = np.zeros((num_eg, vertex, 1))
+    num = int(num_eg/2) 
+    
+    for i in range(num):
+        data["graphs"][i, :, :], data["vertex"][i, :] = one_random_graph_label(vertex, 1/3, 2/3)
+        data["labels"][i, :] = 1
+    ensemble = generate_graphs(num, vertex, 1/2, 0)
+    data["graphs"][num:num_eg, :, :] = ensemble["graphs"]
+    data["labels"][num:num_eg, :] = ensemble["labels"]
+    return data 
+
+def one_random_clique_label(vertex, dens):
+    permutation = list(np.random.permutation(vertex))
+    
+    num1 = int(np.sqrt(vertex)/2)
+    print(num1)
+    G = np.zeros((vertex,vertex))
+    label = np.zeros((vertex,1))
+    label[0:num1,:] = np.ones((num1,1))
+    
+    for i in range(vertex):
+        for j in range(i):
+            G[i,j] = np.random.rand() < dens 
+            G[j,i] = G[i,j] 
+    
+    for i in range(num1):
+        for j in range(i):
+            G[i,j] = 1
+            G[j,i] = 1 
+    
+    G = G[:, permutation]
+    G = G[permutation, :] 
+    label = label[permutation,:] 
+    return G, label 
+
+def generate_ensemble_clique_label(num_eg, vertex):
+    data = {}
+    data["graphs"] = np.zeros((num_eg, vertex, vertex))
+    data["labels"] = np.zeros((num_eg, 1))
+    data["vertex"] = np.zeros((num_eg, vertex, 1))
+    num = int(num_eg/2) 
+    
+    for i in range(num):
+        data["graphs"][i, :, :], data["vertex"][i, :] = one_random_clique_label(vertex, 1/2)
+        data["labels"][i, :] = 1
+    ensemble = generate_graphs(num, vertex, 0.5, 0)
+    data["graphs"][num:num_eg, :, :] = ensemble["graphs"]
+    data["labels"][num:num_eg, :] = ensemble["labels"]
+    return data
+
+def generate_ensemble_same_label(num_eg, vertex):
+    data = {}
+    data["graphs"] = np.zeros((num_eg, vertex, vertex))
+    data["labels"] = np.zeros((num_eg, 1))
+    data["vertex"] = np.zeros((num_eg, vertex, 1))
+    num = int(num_eg/2) 
+    
+    ensemble0 = generate_graphs(num, vertex, 0.5, 1)
+    data["graphs"][range(num), :, :] = ensemble0["graphs"]
+    data["labels"][range(num), :] = 1
+    ensemble = generate_graphs(num, vertex, 0.5, 0)
+    data["graphs"][num:num_eg, :, :] = ensemble["graphs"]
+    data["labels"][num:num_eg, :] = ensemble["labels"]
+    return data
--- random_mini_batches.py
+++ random_mini_batches.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math as math 
 
-def random_mini_batches(X, Y, mini_batch_size = 64):
+def random_mini_batches3(X, Y, Z, mini_batch_size = 64):
     
     m = X.shape[0]                  # number of training examples
     mini_batches = []
@@ -9,13 +9,49 @@ def random_mini_batches(X, Y, mini_batch_size = 64):
     permutation = list(np.random.permutation(m))
     shuffled_X = X[permutation, :]
     shuffled_Y = Y[permutation, :].reshape((m,1))
-
+    shuffled_Z = Z[permutation, :]
     # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
     num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
     for k in range(0, num_complete_minibatches):
         ### START CODE HERE ### (approx. 2 lines)
         mini_batch_X = shuffled_X[range(k * mini_batch_size, (k+1)*mini_batch_size), :]
         mini_batch_Y = shuffled_Y[range(k * mini_batch_size, (k+1)*mini_batch_size), :].reshape((mini_batch_size,1))
+        mini_batch_Z = shuffled_Z[range(k * mini_batch_size, (k+1)*mini_batch_size), :]
+        
+        ### END CODE HERE ###
+        mini_batch = (mini_batch_X, mini_batch_Y, mini_batch_Z)
+        mini_batches.append(mini_batch)
+    
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        ### START CODE HERE ### (approx. 2 lines)
+        mini_batch_X = shuffled_X[range(num_complete_minibatches * mini_batch_size, m), :]
+        mini_batch_Y = shuffled_Y[range(num_complete_minibatches * mini_batch_size, m), :]
+        mini_batch_Z = shuffled_Z[range(num_complete_minibatches * mini_batch_size, m), :]
+        
+        ### END CODE HERE ###
+        mini_batch = (mini_batch_X, mini_batch_Y, mini_batch_Z)
+        mini_batches.append(mini_batch)
+    
+    return mini_batches
+
+def random_mini_batches2(X, Y, mini_batch_size = 64):
+    
+    m = X.shape[0]                  # number of training examples
+    mini_batches = []
+        
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[permutation, :]
+    shuffled_Y = Y[permutation, :].reshape((m,1))
+    
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        ### START CODE HERE ### (approx. 2 lines)
+        mini_batch_X = shuffled_X[range(k * mini_batch_size, (k+1)*mini_batch_size), :]
+        mini_batch_Y = shuffled_Y[range(k * mini_batch_size, (k+1)*mini_batch_size), :].reshape((mini_batch_size,1))
+        
+        
         ### END CODE HERE ###
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
@@ -25,10 +61,97 @@ def random_mini_batches(X, Y, mini_batch_size = 64):
         ### START CODE HERE ### (approx. 2 lines)
         mini_batch_X = shuffled_X[range(num_complete_minibatches * mini_batch_size, m), :]
         mini_batch_Y = shuffled_Y[range(num_complete_minibatches * mini_batch_size, m), :]
+        
         ### END CODE HERE ###
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
     
     return mini_batches
 
+def random_mini_batches_perm(X, Y, mini_batch_size = 64):
+    
+    m = X.shape[0]                  # number of training examples
+    mini_batches = []
+    vertexsq = X.shape[1]
+    vertex = int(np.sqrt(vertexsq))
+    
+    X = X.reshape((-1, vertex, vertex))
+    
+    if True:
+        for i in range(m):
+            permutation = list(np.random.permutation(vertex))
+            graph = X[i,:,:]
+            graph = graph[permutation,:]
+            graph = graph[:,permutation]
+            X[i,:,:] = graph 
+        
+    X = X.reshape((-1,vertexsq))
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[permutation, :]
+    shuffled_Y = Y[permutation, :].reshape((m,1))
+    
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        ### START CODE HERE ### (approx. 2 lines)
+        mini_batch_X = shuffled_X[range(k * mini_batch_size, (k+1)*mini_batch_size), :]
+        mini_batch_Y = shuffled_Y[range(k * mini_batch_size, (k+1)*mini_batch_size), :].reshape((mini_batch_size,1))
+        
+        
+        ### END CODE HERE ###
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        ### START CODE HERE ### (approx. 2 lines)
+        mini_batch_X = shuffled_X[range(num_complete_minibatches * mini_batch_size, m), :]
+        mini_batch_Y = shuffled_Y[range(num_complete_minibatches * mini_batch_size, m), :]
+        
+        ### END CODE HERE ###
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    return mini_batches
 
+def random_mini_batches_conv_perm(X, Y, mini_batch_size = 64):
+    
+    m = X.shape[0]                  # number of training examples
+    mini_batches = []
+    vertex = X.shape[1]
+    
+    if True:
+        for i in range(m):
+            permutation = list(np.random.permutation(vertex))
+            graph = X[i,:,:]
+            graph = graph[permutation,:]
+            graph = graph[:,permutation]
+            X[i,:,:] = graph 
+        
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[permutation, :]
+    shuffled_Y = Y[permutation, :].reshape((m,1))
+    
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        ### START CODE HERE ### (approx. 2 lines)
+        mini_batch_X = shuffled_X[range(k * mini_batch_size, (k+1)*mini_batch_size), :]
+        mini_batch_Y = shuffled_Y[range(k * mini_batch_size, (k+1)*mini_batch_size), :].reshape((mini_batch_size,1))
+        
+        
+        ### END CODE HERE ###
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        ### START CODE HERE ### (approx. 2 lines)
+        mini_batch_X = shuffled_X[range(num_complete_minibatches * mini_batch_size, m), :]
+        mini_batch_Y = shuffled_Y[range(num_complete_minibatches * mini_batch_size, m), :]
+        
+        ### END CODE HERE ###
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    return mini_batches
